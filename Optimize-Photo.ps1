be48d86398a7a8519ce4ae08649971000bb27a44
wub <victorwoo@gmail.com>
2013-12-02 14:33:23 +0800

    Group files by type.

--- Optimize-Photo.ps1
+++ Optimize-Photo.ps1
@@ -4,9 +4,24 @@
 # 3. 旋转照片
 # 4. 输出时遇到重名文件：1. 比较 2. 改名。
 
+$inputDir = 'E:\Photo\optimize'
+cd D:\sync\vichamp\Optimize-Photo
+
 $DebugPreference = "Continue"
 . .\get-exif.ps1
 . .\Get-Image.ps1
+
+function Get-FileType ($ext) {
+    if ('.avi', '.mov', '.3gp' -contains $ext) {
+        return 'video'
+    }
+
+    if ('.jpg', '.png', '.bmp' -contains $ext) {
+        return 'photo'
+    }
+
+    return 'unknown'
+}
 
 function Rename-Photo ($file) {
     # http://archive.msdn.microsoft.com/PSImage
@@ -32,29 +47,29 @@ function Rename-Photo ($file) {
     # Android 自带相机照片
     # 2010-07-21 13.25.54.jpg
     # 2010-08-08 06.32.34[0].jpgz
-    if (!$matched) { $matched = $baseName -cmatch '^(?<YEAR>\d{4})-(?<MONTH>\d{2})-(?<DAY>\d{2}) (?<HOUR>\d{2})\.(?<MINUTE>\d{2})\.(?<SECOND>\d{2})(?:\[\d+\])?$' }
+    if (!$matched) { $matched = $baseName -cmatch '^(?<YEAR>\d{4})-(?<MONTH>\d{2})-(?<DAY>\d{2}) (?<HOUR>\d{2})\.(?<MINUTE>\d{2})\.(?<SECOND>\d{2})(?:\[\d+\])?\s*(?<REST>.*)$' }
 
     # Camera 360 相机
     # C360_2010-09-30 10-55-52.jpg
     # C360_2010-09-30 10-56-34.Share.jpg
     # C360_2010-10-02 20-20-50-1.jpg
-    if (!$matched) { $matched = $baseName -cmatch '^C360_(?<YEAR>\d{4})-(?<MONTH>\d{2})-(?<DAY>\d{2}) (?<HOUR>\d{2})-(?<MINUTE>\d{2})-(?<SECOND>\d{2})(?:\.|-)?(?<REST>\w*)$' }
+    if (!$matched) { $matched = $baseName -cmatch '^C360_(?<YEAR>\d{4})-(?<MONTH>\d{2})-(?<DAY>\d{2}) (?<HOUR>\d{2})-(?<MINUTE>\d{2})-(?<SECOND>\d{2})\s*(?<REST>.*)$' }
 
     # 疑似 Android 自带相机视频
     # video-2010-08-27-21-53-27.3gp
-    if (!$matched) { $matched = $baseName -cmatch '^video-(?<YEAR>\d{4})-(?<MONTH>\d{2})-(?<DAY>\d{2})-(?<HOUR>\d{2})-(?<MINUTE>\d{2})-(?<SECOND>\d{2})$' }
+    if (!$matched) { $matched = $baseName -cmatch '^video-(?<YEAR>\d{4})-(?<MONTH>\d{2})-(?<DAY>\d{2})-(?<HOUR>\d{2})-(?<MINUTE>\d{2})-(?<SECOND>\d{2})\s*(?<REST>.*)$' }
         
     # 百度云同步盘
     # 2013-09-11 234206(1).jpg
     # 2013-09-11 234206.jpg
     # 2013-08-10 192913.mov
-    if (!$matched) { $matched = $baseName -cmatch '^(?<YEAR>\d{4})-(?<MONTH>\d{2})-(?<DAY>\d{2}) (?<HOUR>\d{2})(?<MINUTE>\d{2})(?<SECOND>\d{2})(?<REST>.*)$' }
+    if (!$matched) { $matched = $baseName -cmatch '^(?<YEAR>\d{4})-(?<MONTH>\d{2})-(?<DAY>\d{2}) (?<HOUR>\d{2})(?<MINUTE>\d{2})(?<SECOND>\d{2})\s*(?<REST>.*)$' }
 
     # 微云
     # 20131113101746588.JPG
     # 20131113101746588(0).JPG
     # 20131113101746797.JPG
-    if (!$matched) { $matched = $baseName -cmatch '^(?<YEAR>\d{4})(?<MONTH>\d{2})(?<DAY>\d{2})(?<HOUR>\d{2})(?<MINUTE>\d{2})(?<SECOND>\d{2})(?<MILLISECOND>\d{3})(?:\((?<REST>).*\))?$' }
+    if (!$matched) { $matched = $baseName -cmatch '^(?<YEAR>\d{4})(?<MONTH>\d{2})(?<DAY>\d{2})(?<HOUR>\d{2})(?<MINUTE>\d{2})(?<SECOND>\d{2})(?<MILLISECOND>\d{3})\s*(?<REST>.*)$' }
 
     # 从 Exif 或者文件名中取到时间
     if ($matched) {
@@ -86,7 +101,7 @@ function Rename-Photo ($file) {
 
     if ($matched) {
         # 若通过 exif 或文件名识别出了时间信息
-        $newName = "{0:d4}-{1:d2}-{2:d2} {3:d2}.{4:d2}.{5:d2}" -f $year, $month, $day, $hour, $minute, $second
+        $newBaseName = "{0:d4}-{1:d2}-{2:d2} {3:d2}.{4:d2}.{5:d2}" -f $year, $month, $day, $hour, $minute, $second
         if ($rest) {
             $rest = $rest.Trim()
             $rest = $rest.TrimStart('-')
@@ -94,24 +109,46 @@ function Rename-Photo ($file) {
             $rest = $rest.TrimEnd(')')
             $rest = $rest.TrimStart('[')
             $rest = $rest.TrimEnd(']')
-
-            $newName = "{0} [1]" -f $newName, $rest
+            if ($rest -cnotmatch '\d+') {
+                $newBaseName = "{0} [1]" -f $newBaseName, $rest
+            }
         }
 
-        $newName = "{0}{1}" -f $newName, $extension
-        echo "$fileName`t$newName"
-        move -LiteralPath $_.FullName "BYDATE\$newName" -WhatIf
+        $newFileName = "{0}{1}" -f $newBaseName, $extension
+        
+        $fileType = Get-FileType $extension
+        $folder = "$fileType.bydate"
+        if (!(Test-Path $folder)) {
+            md $folder | Out-Null
+        }
+
+        if (Test-Path "$folder\$newFileName") {
+            for ($i = 1;;$i++) {
+                $newFileName = "$newBaseName ($i)$extension"
+                if (!(Test-Path "$folder\$newFileName")) {
+                    break
+                }
+            }
+        }
+        echo "$fileName -> $newFileName"
+        move -LiteralPath $_.FullName "$folder\$newFileName" #-WhatIf
     } else {
         Write-Warning "无法识别 $fileName"
         if (!(Test-Path 'UNKNOWN')) {
-            md 'UNKNOWN'
+            md 'UNKNOWN' | Out-Null
         }
-        move -LiteralPath $_.FullName UNKNOWN -WhatIf 
+        move -LiteralPath $_.FullName UNKNOWN #-WhatIf 
     }
 }
 
-
-
-dir *.jpg, *.png, *.avi, *.mov | foreach {
+pushd
+cd $inputDir
+dir -Recurse *.jpg, *.png, *.avi, *.mov, *.3gp | 
+    where { -not $_.Directory.Name.Contains('bydate') -and -not $_.Directory.Name.Contains('unknown') } | foreach {
     Rename-Photo $_
 }
+
+(Get-ChildItem -recurse | Where-Object {$_.PSIsContainer -eq $True}) |
+    Where-Object {$_.GetFiles().Count -eq 0} | Remove-Item
+
+popd
